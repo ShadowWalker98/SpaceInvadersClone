107ac62db5b29c217af479f21378232c1a1b75d1
Satyajit Mohanty <swordhollow@gmail.com>
2025-08-04 18:36:30 -0400

    added name support using map and added movement constants

--- main.cpp
+++ main.cpp
@@ -1,116 +1,118 @@
 
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include<unordered_map>
 
-#define LEFTX -10
-#define RIGHTX 10
-#define UPY -10
-#define DOWNY 10
-std::string getKeyName(sf::Keyboard::Key key) {
-    switch (key) {
-        case sf::Keyboard::Key::A: return "A";
-        case sf::Keyboard::Key::B: return "B";
-        case sf::Keyboard::Key::C: return "C";
-        case sf::Keyboard::Key::D: return "D";
-        case sf::Keyboard::Key::E: return "E";
-        case sf::Keyboard::Key::F: return "F";
-        case sf::Keyboard::Key::G: return "G";
-        case sf::Keyboard::Key::H: return "H";
-        case sf::Keyboard::Key::I: return "I";
-        case sf::Keyboard::Key::J: return "J";
-        case sf::Keyboard::Key::K: return "K";
-        case sf::Keyboard::Key::L: return "L";
-        case sf::Keyboard::Key::M: return "M";
-        case sf::Keyboard::Key::N: return "N";
-        case sf::Keyboard::Key::O: return "O";
-        case sf::Keyboard::Key::P: return "P";
-        case sf::Keyboard::Key::Q: return "Q";
-        case sf::Keyboard::Key::R: return "R";
-        case sf::Keyboard::Key::S: return "S";
-        case sf::Keyboard::Key::T: return "T";
-        case sf::Keyboard::Key::U: return "U";
-        case sf::Keyboard::Key::V: return "V";
-        case sf::Keyboard::Key::W: return "W";
-        case sf::Keyboard::Key::X: return "X";
-        case sf::Keyboard::Key::Y: return "Y";
-        case sf::Keyboard::Key::Z: return "Z";
-        case sf::Keyboard::Key::Num0: return "Num0";
-        case sf::Keyboard::Key::Num1: return "Num1";
-        case sf::Keyboard::Key::Num2: return "Num2";
-        case sf::Keyboard::Key::Num3: return "Num3";
-        case sf::Keyboard::Key::Num4: return "Num4";
-        case sf::Keyboard::Key::Num5: return "Num5";
-        case sf::Keyboard::Key::Num6: return "Num6";
-        case sf::Keyboard::Key::Num7: return "Num7";
-        case sf::Keyboard::Key::Num8: return "Num8";
-        case sf::Keyboard::Key::Num9: return "Num9";
-        case sf::Keyboard::Key::Escape: return "Escape";
-        case sf::Keyboard::Key::LControl: return "LControl";
-        case sf::Keyboard::Key::LShift: return "LShift";
-        case sf::Keyboard::Key::LAlt: return "LAlt";
-        case sf::Keyboard::Key::LSystem: return "LSystem";
-        case sf::Keyboard::Key::RControl: return "RControl";
-        case sf::Keyboard::Key::RShift: return "RShift";
-        case sf::Keyboard::Key::RAlt: return "RAlt";
-        case sf::Keyboard::Key::RSystem: return "RSystem";
-        case sf::Keyboard::Key::Menu: return "Menu";
-        case sf::Keyboard::Key::LBracket: return "LBracket";
-        case sf::Keyboard::Key::RBracket: return "RBracket";
-        case sf::Keyboard::Key::Semicolon: return "Semicolon";
-        case sf::Keyboard::Key::Comma: return "Comma";
-        case sf::Keyboard::Key::Period: return "Period";
-        case sf::Keyboard::Key::Apostrophe: return "Apostrophe";
-        case sf::Keyboard::Key::Slash: return "Slash";
-        case sf::Keyboard::Key::Backslash: return "Backslash";
-        case sf::Keyboard::Key::Equal: return "Equal";
-        case sf::Keyboard::Key::Hyphen: return "Hyphen";
-        case sf::Keyboard::Key::Space: return "Space";
-        case sf::Keyboard::Key::Enter: return "Enter";
-        case sf::Keyboard::Key::Backspace: return "Backspace";
-        case sf::Keyboard::Key::Tab: return "Tab";
-        case sf::Keyboard::Key::PageUp: return "PageUp";
-        case sf::Keyboard::Key::PageDown: return "PageDown";
-        case sf::Keyboard::Key::End: return "End";
-        case sf::Keyboard::Key::Home: return "Home";
-        case sf::Keyboard::Key::Insert: return "Insert";
-        case sf::Keyboard::Key::Delete: return "Delete";
-        case sf::Keyboard::Key::Add: return "Add";
-        case sf::Keyboard::Key::Subtract: return "Subtract";
-        case sf::Keyboard::Key::Multiply: return "Multiply";
-        case sf::Keyboard::Key::Divide: return "Divide";
-        case sf::Keyboard::Key::Left: return "Left";
-        case sf::Keyboard::Key::Right: return "Right";
-        case sf::Keyboard::Key::Up: return "Up";
-        case sf::Keyboard::Key::Down: return "Down";
-        case sf::Keyboard::Key::Numpad0: return "Numpad0";
-        case sf::Keyboard::Key::Numpad1: return "Numpad1";
-        case sf::Keyboard::Key::Numpad2: return "Numpad2";
-        case sf::Keyboard::Key::Numpad3: return "Numpad3";
-        case sf::Keyboard::Key::Numpad4: return "Numpad4";
-        case sf::Keyboard::Key::Numpad5: return "Numpad5";
-        case sf::Keyboard::Key::Numpad6: return "Numpad6";
-        case sf::Keyboard::Key::Numpad7: return "Numpad7";
-        case sf::Keyboard::Key::Numpad8: return "Numpad8";
-        case sf::Keyboard::Key::Numpad9: return "Numpad9";
-        case sf::Keyboard::Key::F1: return "F1";
-        case sf::Keyboard::Key::F2: return "F2";
-        case sf::Keyboard::Key::F3: return "F3";
-        case sf::Keyboard::Key::F4: return "F4";
-        case sf::Keyboard::Key::F5: return "F5";
-        case sf::Keyboard::Key::F6: return "F6";
-        case sf::Keyboard::Key::F7: return "F7";
-        case sf::Keyboard::Key::F8: return "F8";
-        case sf::Keyboard::Key::F9: return "F9";
-        case sf::Keyboard::Key::F10: return "F10";
-        case sf::Keyboard::Key::F11: return "F11";
-        case sf::Keyboard::Key::F12: return "F12";
-        case sf::Keyboard::Key::F13: return "F13";
-        case sf::Keyboard::Key::F14: return "F14";
-        case sf::Keyboard::Key::F15: return "F15";
-        case sf::Keyboard::Key::Pause: return "Pause";
-        default: return "Unknown";
-    }
-}
+
+
+constexpr float MOVEMENT_SPEED = 10.0f;
+constexpr float MOVE_LEFT = -MOVEMENT_SPEED;
+constexpr float MOVE_RIGHT = MOVEMENT_SPEED;
+constexpr float MOVE_UP = -MOVEMENT_SPEED;
+constexpr float MOVE_DOWN = MOVEMENT_SPEED;
+
+const std::unordered_map<sf::Keyboard::Key, std::string> keyNameMap = {
+        {sf::Keyboard::Key::A, "A"},
+        {sf::Keyboard::Key::B, "B"},
+        {sf::Keyboard::Key::C, "C"},
+        {sf::Keyboard::Key::D, "D"},
+        {sf::Keyboard::Key::E, "E"},
+        {sf::Keyboard::Key::F, "F"},
+        {sf::Keyboard::Key::G, "G"},
+        {sf::Keyboard::Key::H, "H"},
+        {sf::Keyboard::Key::I, "I"},
+        {sf::Keyboard::Key::J, "J"},
+        {sf::Keyboard::Key::K, "K"},
+        {sf::Keyboard::Key::L, "L"},
+        {sf::Keyboard::Key::M, "M"},
+        {sf::Keyboard::Key::N, "N"},
+        {sf::Keyboard::Key::O, "O"},
+        {sf::Keyboard::Key::P, "P"},
+        {sf::Keyboard::Key::Q, "Q"},
+        {sf::Keyboard::Key::R, "R"},
+        {sf::Keyboard::Key::S, "S"},
+        {sf::Keyboard::Key::T, "T"},
+        {sf::Keyboard::Key::U, "U"},
+        {sf::Keyboard::Key::V, "V"},
+        {sf::Keyboard::Key::W, "W"},
+        {sf::Keyboard::Key::X, "X"},
+        {sf::Keyboard::Key::Y, "Y"},
+        {sf::Keyboard::Key::Z, "Z"},
+        {sf::Keyboard::Key::Num0, "Num0"},
+        {sf::Keyboard::Key::Num1, "Num1"},
+        {sf::Keyboard::Key::Num2, "Num2"},
+        {sf::Keyboard::Key::Num3, "Num3"},
+        {sf::Keyboard::Key::Num4, "Num4"},
+        {sf::Keyboard::Key::Num5, "Num5"},
+        {sf::Keyboard::Key::Num6, "Num6"},
+        {sf::Keyboard::Key::Num7, "Num7"},
+        {sf::Keyboard::Key::Num8, "Num8"},
+        {sf::Keyboard::Key::Num9, "Num9"},
+        {sf::Keyboard::Key::Escape, "Escape"},
+        {sf::Keyboard::Key::LControl, "LControl"},
+        {sf::Keyboard::Key::LShift, "LShift"},
+        {sf::Keyboard::Key::LAlt, "LAlt"},
+        {sf::Keyboard::Key::LSystem, "LSystem"},
+        {sf::Keyboard::Key::RControl, "RControl"},
+        {sf::Keyboard::Key::RShift, "RShift"},
+        {sf::Keyboard::Key::RAlt, "RAlt"},
+        {sf::Keyboard::Key::RSystem, "RSystem"},
+        {sf::Keyboard::Key::Menu, "Menu"},
+        {sf::Keyboard::Key::LBracket, "LBracket"},
+        {sf::Keyboard::Key::RBracket, "RBracket"},
+        {sf::Keyboard::Key::Semicolon, "Semicolon"},
+        {sf::Keyboard::Key::Comma, "Comma"},
+        {sf::Keyboard::Key::Period, "Period"},
+        {sf::Keyboard::Key::Apostrophe, "Apostrophe"},
+        {sf::Keyboard::Key::Slash, "Slash"},
+        {sf::Keyboard::Key::Backslash, "Backslash"},
+        {sf::Keyboard::Key::Equal, "Equal"},
+        {sf::Keyboard::Key::Hyphen, "Hyphen"},
+        {sf::Keyboard::Key::Space, "Space"},
+        {sf::Keyboard::Key::Enter, "Enter"},
+        {sf::Keyboard::Key::Backspace, "Backspace"},
+        {sf::Keyboard::Key::Tab, "Tab"},
+        {sf::Keyboard::Key::PageUp, "PageUp"},
+        {sf::Keyboard::Key::PageDown, "PageDown"},
+        {sf::Keyboard::Key::End, "End"},
+        {sf::Keyboard::Key::Home, "Home"},
+        {sf::Keyboard::Key::Insert, "Insert"},
+        {sf::Keyboard::Key::Delete, "Delete"},
+        {sf::Keyboard::Key::Add, "Add"},
+        {sf::Keyboard::Key::Subtract, "Subtract"},
+        {sf::Keyboard::Key::Multiply, "Multiply"},
+        {sf::Keyboard::Key::Divide, "Divide"},
+        {sf::Keyboard::Key::Left, "Left"},
+        {sf::Keyboard::Key::Right, "Right"},
+        {sf::Keyboard::Key::Up, "Up"},
+        {sf::Keyboard::Key::Down, "Down"},
+        {sf::Keyboard::Key::Numpad0, "Numpad0"},
+        {sf::Keyboard::Key::Numpad1, "Numpad1"},
+        {sf::Keyboard::Key::Numpad2, "Numpad2"},
+        {sf::Keyboard::Key::Numpad3, "Numpad3"},
+        {sf::Keyboard::Key::Numpad4, "Numpad4"},
+        {sf::Keyboard::Key::Numpad5, "Numpad5"},
+        {sf::Keyboard::Key::Numpad6, "Numpad6"},
+        {sf::Keyboard::Key::Numpad7, "Numpad7"},
+        {sf::Keyboard::Key::Numpad8, "Numpad8"},
+        {sf::Keyboard::Key::Numpad9, "Numpad9"},
+        {sf::Keyboard::Key::F1, "F1"},
+        {sf::Keyboard::Key::F2, "F2"},
+        {sf::Keyboard::Key::F3, "F3"},
+        {sf::Keyboard::Key::F4, "F4"},
+        {sf::Keyboard::Key::F5, "F5"},
+        {sf::Keyboard::Key::F6, "F6"},
+        {sf::Keyboard::Key::F7, "F7"},
+        {sf::Keyboard::Key::F8, "F8"},
+        {sf::Keyboard::Key::F9, "F9"},
+        {sf::Keyboard::Key::F10, "F10"},
+        {sf::Keyboard::Key::F11, "F11"},
+        {sf::Keyboard::Key::F12, "F12"},
+        {sf::Keyboard::Key::F13, "F13"},
+        {sf::Keyboard::Key::F14, "F14"},
+        {sf::Keyboard::Key::F15, "F15"},
+        {sf::Keyboard::Key::Pause, "Pause"}
+};
 
 void renderStep(sf::RenderWindow *currentWindow, sf::Sprite *sprite) {
     (*currentWindow).clear();
@@ -143,40 +145,39 @@ int main() {
     while (window.isOpen()) {
 
         while(const std::optional event = window.pollEvent()) {
+
+            if(!event.has_value()) {
+                continue;
+            }
             if(event->is<sf::Event::Closed>()) {
                 window.close();
             } else if(event->is<sf::Event::KeyPressed>()) {
                 auto keyEvent = event->getIf<sf::Event::KeyPressed>();
-                std::cout << "Key you have pressed is: " << getKeyName(keyEvent->code) << "\n";
                 sf::FloatRect bounds = sprite.getGlobalBounds();
                 if(keyEvent->code == sf::Keyboard::Key::Left) {
-                    sf::Vector2<float> leftTransform(LEFTX, 0);
+                    sf::Vector2<float> leftTransform(MOVE_LEFT, 0);
                     if(bounds.position.x + leftTransform.x < 0) {
                         leftTransform.x = 0;
                     }
                     sprite.move(leftTransform);
-                    std::cout << "Moving left\n";
                 } else if(keyEvent->code == sf::Keyboard::Key::Right) {
-                    sf::Vector2<float> rightTransform(RIGHTX, 0);
+                    sf::Vector2<float> rightTransform(MOVE_RIGHT, 0);
                     if(bounds.position.x + bounds.size.x + rightTransform.x > (float) window.getSize().x) {
                         rightTransform.x = 0;
                     }
                     sprite.move(rightTransform);
-                    std::cout << "Moving right\n";
                 } else if(keyEvent->code == sf::Keyboard::Key::Up) {
-                    sf::Vector2<float> upTransform(0, UPY);
+                    sf::Vector2<float> upTransform(0, MOVE_UP);
                     if(bounds.position.y + upTransform.y < 0) {
                         upTransform.y = 0;
                     }
-                    std::cout << "Moving up\n";
                     sprite.move(upTransform);
                 } else if(keyEvent->code == sf::Keyboard::Key::Down) {
-                    sf::Vector2<float> downTransform(0, DOWNY);
+                    sf::Vector2<float> downTransform(0, MOVE_DOWN);
                     if(bounds.position.y + bounds.size.y + downTransform.y > (float) window.getSize().y) {
                         downTransform.y = 0;
                     }
                     sprite.move(downTransform);
-                    std::cout << "Moving down\n";
                 }
 
 
